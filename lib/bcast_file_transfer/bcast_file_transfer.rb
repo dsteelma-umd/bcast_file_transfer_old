0069ab09642ab5d95af139350565e1095384afe1
David P. Steelman <dsteelma@umd.edu>
2018-01-08 08:53:52 -0500

    Moved mail functionality into separate class.

--- lib/bcast_file_transfer/bcast_file_transfer.rb
+++ lib/bcast_file_transfer/bcast_file_transfer.rb
@@ -105,26 +105,8 @@ module BcastFileTransfer
       logger
     end
 
-    def send_email(script_result)
-    #  comparison_results = script_result.comparison_results
-    #  transfer_results = script_result.transfer_results
-
-    #  (successful_transfers, failed_transfers) = transfer_results.partition(&:success?)
-
-      email = ''
-
-      if script_result.success?
-        email = File.read(
-          File.join(File.dirname(File.expand_path(__FILE__)), '../../resources/mail_templates/success.erb')
-        )
-      else
-        email = File.read(
-          File.join(File.dirname(File.expand_path(__FILE__)), '../../resources/mail_templates/failure.erb')
-        )
-      end
-
-      email_text = ERB.new(email, 0, '>').result(binding)
-      puts "ERB:\n\n #{email_text}"
+    def send_mail(config_hash, script_result)
+      Mail.send_mail(config_hash, script_result)
     end
   end
 end
