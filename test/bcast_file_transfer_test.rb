35dbebbb7acbe39a86183dededbe1dcd46df0c6e
David P. Steelman <dsteelma@umd.edu>
2018-01-10 13:27:38 -0500

    Removed useless test.

--- test/bcast_file_transfer_test.rb
+++ test/bcast_file_transfer_test.rb
@@ -4,8 +4,4 @@ class BcastFileTransferTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::BcastFileTransfer::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
